cc67b5a4e7bef7b516b8fd8d87d964e221f7a3ce
Dorel Cohen <yourEmail@example.com>
2025-12-17 00:05:06 +0200

    rook is 100%

--- part a/main.cpp
+++ part a/main.cpp
@@ -5,20 +5,55 @@ int main()
 {
     std::cout << "Chess pieces module loaded successfully." << std::endl;
     // some tests:
-    
+
     // rook
     rook test_rook("a1", true);
-    std::cout << test_rook.give_location() << std::endl; // see if location is set correctly
-    std::cout << test_rook.get_is_alive() << std::endl;  // see if is_alive is true
+
+    if(test_rook.give_location() == "a1" && test_rook.get_is_alive() == true && test_rook.get_is_white() == true)
+    {
+        std::cout << "Rook make tests passed." << std::endl;
+    }
+    else
+    {
+        std::cout << "Rook make tests failed." << std::endl;
+    }
     test_rook.set_destination("a4");
-    std::cout << test_rook.get_destination() << std::endl; // see if destination is set correctly
-    if (test_rook.give_location() == "a1" && test_rook.get_is_alive() == true && test_rook.get_destination() == "a4") 
+    if(test_rook.get_destination() == "a4")
     {
-        std::cout << "Rook tests passed." << std::endl;
-    } 
-    else 
+        std::cout << "Rook destination set correctly." << std::endl;
+    }
+    else
     {
-        std::cout << "Rook tests failed." << std::endl;
+        std::cout << "Rook destination not set correctly." << std::endl;
+    }
+    MoveResult result = test_rook.is_move_ok("R#####rr########################################################1");
+    if(result == MoveResult::Valid_Checkmate)
+    {
+        std::cout << "Rook move validation passed." << std::endl;
+    }
+    else
+    {
+        std::cout << "got: " << std::endl;
+        std::cout << result << std::endl;
+        std::cout << "Rook move validation failed." << std::endl;
+    }   
+    test_rook.move();
+    if(test_rook.give_location() == "a4")
+    {
+        std::cout << "Rook move executed correctly." << std::endl;
+    }
+    else
+    {
+        std::cout << "Rook move execution failed." << std::endl;
+    }
+    test_rook.set_die();
+    if(test_rook.get_is_alive() == false)
+    {
+        std::cout << "Rook set_die() test passed." << std::endl;
+    }
+    else
+    {
+        std::cout << "Rook set_die() test failed. \n\n\n" << std::endl;
     }
     // end of tests for rook
 
