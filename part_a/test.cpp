75c01d71a3a3b161fc92d01fba2cf44ef64d26a5
Dorel Cohen <yourEmail@example.com>
2025-12-21 22:29:34 +0200

    add todo.cpp

--- part_a/test.cpp
+++ part_a/test.cpp
@@ -9,15 +9,6 @@ void test_pawn();
 void test_king();  
 void test_rook_and_king();
  
-//! fix the bool is_there_check(std::string state_of_board); logic
-
-//! fix the king is_move_ok
-
-//! fix the rook is_move_ok
-
-//! fix the rook use of is_there_check
-
-//! fix the king use of is_there_check
 
 int main() 
 {
---
+++ todo.cpp
@@ -0,0 +1,13 @@
+//! fix the bool is_there_check(std::string state_of_board); logic
+
+//! fix the king is_move_ok
+
+//! fix the rook is_move_ok
+
+//! fix the rook use of is_there_check
+
+//! fix the king use of is_there_check
+
+//TODO get the board class runing
+
+//TODO get 
