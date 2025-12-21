e6c88e1e9cfb3d112924ac5d09bf8efee83005ad
Dorel Cohen <yourEmail@example.com>
2025-12-22 00:21:48 +0200

    debug

--- todo.cpp
+++ todo.cpp
@@ -13,3 +13,24 @@
 
 //TODO get all the chess pieces runing 
 
+//? will it get if the rook goes to b8 or c8 instade? maby its with the index and string changes we have? 
+//-note-> see if 64 - [8 -> 7(problem becouse index from 0)] gets the less line to be only {b8 <--to--> h8} or mabye {B8 <--to--> G8}
+
+/*
+//+ test out +//////////////////////////////////////////////////////////////////////////////////////////////
+//|=== Rook + King endgame test ===
+//|Initial board:
+//|R###K#######################################################k###0
+//!Move 1: White rook a1 -> a8: 0 ✗ (Expected 1=Valid_Check)
+//|Board after move 1:
+//|####K###################################################R###k###1
+//!Move 2: Black king e8 -> d8: 0 ✗ (Expected 4=Invalid_SelfCheck)
+//|Move 2 alt: Black king e8 -> f7: 0 ✓ (Valid)
+//|Board after move 2:
+//|####K################################################k##R#######0
+//|Move 3: White king e1 -> e2: 0 ✓ (Valid)
+//|Board after move 3:
+//|############K########################################k##R#######1
+//!Move 4: White rook a8 -> f8: 6 ✗ (Unexpected result)
+//|=== End of test ===
+*/
