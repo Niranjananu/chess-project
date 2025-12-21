d0cbf8f2b9fdb2fb9b0b184789a2393123674786
Dorel Cohen <yourEmail@example.com>
2025-12-21 22:36:08 +0200

    format

--- todo.cpp
+++ todo.cpp
@@ -1,22 +1,15 @@
-//+ part 1 +//
+//+ part 1 +////////////////////////////////////////////////////////////////////////////////////////////////
 
 //! fix the bool is_there_check(std::string state_of_board); logic
-
 //! fix the king is_move_ok
-
 //! fix the rook is_move_ok
-
 //! fix the rook use of is_there_check
-
 //! fix the king use of is_there_check
-
 //TODO get the board class runing
-
 //TODO get the cmd output runing or the frontend 
-
 //TODO make the main.cpp logic and run a working game
 
-//+ part 2 +//
+//+ part 2 +////////////////////////////////////////////////////////////////////////////////////////////////
 
 //TODO get all the chess pieces runing 
 
