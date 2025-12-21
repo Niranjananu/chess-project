059ce357ce2cd2dd0072b6439f793c2dde7c314b
Dorel Cohen <yourEmail@example.com>
2025-12-22 00:09:48 +0200

    fix 4 things from the error board

--- todo.cpp
+++ todo.cpp
@@ -3,8 +3,8 @@
 //! fix the bool is_there_check(std::string state_of_board); logic
 //x// fix the king is_move_ok
 //x// fix the rook is_move_ok
-//! fix the rook use of is_there_check
-//! fix the king use of is_there_check
+//x// fix the rook use of is_there_check
+//x// fix the king use of is_there_check
 //TODO get the board class runing
 //TODO get the cmd output runing or the frontend 
 //TODO make the main.cpp logic and run a working game
