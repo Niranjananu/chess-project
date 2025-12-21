cdf79735ee97c8b769c2a8c6fd689653857c8a1b
Dorel Cohen <yourEmail@example.com>
2025-12-21 22:27:23 +0200

    point out errors

--- part_a/test.cpp
+++ part_a/test.cpp
@@ -9,6 +9,16 @@ void test_pawn();
 void test_king();  
 void test_rook_and_king();
  
+//! fix the bool is_there_check(std::string state_of_board); logic
+
+//! fix the king is_move_ok
+
+//! fix the rook is_move_ok
+
+//! fix the rook use of is_there_check
+
+//! fix the king use of is_there_check
+
 int main() 
 {
     std::cout << "Chess pieces module loaded successfully.\n" << std::endl;
